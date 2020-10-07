6dbf028d3adef19a3185036977d1a08b7c6ac7ac
Arekso <arek.soulahian@gmail.com>
2020-10-07 15:07:46 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,93 +1,33 @@
-//calculator screen
-//screen.textContent = 
+// select buttons
+const buttons = document.querySelectorAll('input');
+// select the input element for display
+const display = document.querySelector('.display');
 
-//buttons
-const btn1 = document.querySelector('#btn1');
-btn1.addEventListener('click', () => {parseInt("1")});
-
-const btn2 = document.querySelector('#btn2');
-btn1.addEventListener('click', () => {parseInt("2")});
-
-const btn3 = document.querySelector('#btn3');
-btn1.addEventListener('click', () => {parseInt("3")});
-
-const btn4 = document.querySelector('#btn4');
-btn1.addEventListener('click', () => {parseInt("4")});
-
-const btn5 = document.querySelector('#btn5');
-btn1.addEventListener('click', () => {parseInt("5")});
-
-const btn6 = document.querySelector('#btn6');
-btn1.addEventListener('click', () => {parseInt("6")});
-
-const btn7 = document.querySelector('#btn7');
-btn1.addEventListener('click', () => {parseInt("7")});
-
-const btn8 = document.querySelector('#btn8');
-btn1.addEventListener('click', () => {parseInt("8")});
-
-const btn9 = document.querySelector('#btn9');
-btn1.addEventListener('click', () => {parseInt("9")});
-
-const btn0 = document.querySelector('#btn0');
-btn1.addEventListener('click', () => {parseInt("0")});
-
-const btnp = document.querySelector('#btnp');
-btn1.addEventListener('click', () => {add()});
-
-const btns = document.querySelector('#btns');
-btn1.addEventListener('click', () => {subtract()});
-
-const btnx = document.querySelector('#btnx');
-btn1.addEventListener('click', () => {multiply()});
-
-const btn0 = document.querySelector('#btn0');
-btn1.addEventListener('click', () => {parseInt("0")});
-
-function calculate (a, op, b) {
-    this.a = a;
-    this.b = b;
-
-}
+//add event listeners for each button
+buttons.forEach(function(button) {
+	button.addEventListener ('click', calculate) ;
+}); 
 
 
+// calculate 
+function calculate(event) {
+	//button clicked value
+	const clickedbutton = event.target.value;
 
-function add (a,b) {
-	return a + b;
-}
-
-function subtract (a,b) {
-	return a - b;
-}
-
-function sum (array) {
-	//reduce adds up all numbers
-	return array.reduce((total, current) => total+current, 0);
-}
-
-function multiply (array) {
-	return array.length
-	? array.reduce((accumulator, nextItem) => accumulator*nextItem)
-	: 0;
-}
-
-function power(a, b) {
-	return Math.pow(a,b);
-}
-
-function factorial(n) {
-	if (n <= 1) {
-		return 1;
+	if (clickedbutton === '=') {
+		//if the value is not empty
+		if (display.value !== '') {
+			display.value = eval(display.value);
+		}
 	}
-	let fac = n*factorial(n-1); 
-	return fac;
-}
+	
+	else if (clickedbutton === 'C') {
+		//clear
+		display.value = '';
+	}
 
-module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
+	else {
+		// concat the clicked button values
+		display.value += clickedbutton;
+	}
 }
